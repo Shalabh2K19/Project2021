3436d7484dea7415842785b55ae302da416eb5cc
unknown <shalabh.bits@gmail.com>
2021-05-09 00:32:39 +0530

    initial commit

---
+++ MySweetProgram/src/MySweetProgram.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MySweetProgram {
+
+    public static void main(String[] args) //method
+    {
+        System.out.println("Hello World");
+
+        Scanner in = new Scanner (System.in);
+        String s = in.nextLine();
+
+        System.out.println(s);
+    }
+}
+
+
+//class
